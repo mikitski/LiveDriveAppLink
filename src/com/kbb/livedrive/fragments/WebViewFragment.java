517fb63ec03cb1e24538b0b183b3d81c66af18f3
Carlos Rendon <crendon@gmail.com>
2014-11-13 12:03:47 -0800

    Displays a score module in app

--- src/com/kbb/livedrive/fragments/WebViewFragment.java
+++ src/com/kbb/livedrive/fragments/WebViewFragment.java
@@ -8,6 +8,7 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.leaderboard.Leaderboard;
 import com.google.android.gms.games.leaderboard.LeaderboardScore;
 import com.google.android.gms.games.leaderboard.LeaderboardScoreBuffer;
+import com.google.android.gms.games.leaderboard.LeaderboardVariantRef;
 import com.google.android.gms.games.leaderboard.Leaderboards;
 import com.google.android.gms.games.leaderboard.Leaderboards.LoadPlayerScoreResult;
 import com.google.android.gms.games.leaderboard.Leaderboards.LoadScoresResult;
@@ -49,6 +50,8 @@ public class WebViewFragment extends BaseFragment {
 				String scoreDisplay = score.getDisplayScore();
 			}
 			
+			// TODO, use real score
+			leaderboardView.loadUrl("javascript:draw(75,80);");
 			
 						
 		}
